453fd895567f8440ba086f331eaf0c5a07cde51b
Yasir323 <yasir.jafri007@gmail.com>
2022-07-18 11:28:31 +0000

    FastAPI - Path Opeartion Configuration

---
+++ fastApi/18pathOperationConfig/statusCode.py
@@ -0,0 +1,20 @@
+from typing import Set, Union
+
+from fastapi import FastAPI, status
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+    tags: Set[str] = set()
+
+
+# Response status code
+@app.post("/items/", response_model=Item, tags=["items"], status_code=status.HTTP_201_CREATED)
+async def create_item(item: Item):
+    return item
